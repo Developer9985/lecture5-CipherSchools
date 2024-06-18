3c11aa9c0dadc181472ed609ba52f1d09e2bca81
Developer9985 <94001072+Developer9985@users.noreply.github.com>
2024-06-18 19:12:48 +0530

    Add files via upload

---
+++ lecture5.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b , c , d;
+
+    cout<<" Eneter four numbers a  b c d"<<endl;
+    cin>>a>>b>>c>>d;
+
+    cout<<(a>b && c > d )<<endl;
+
+    // This program will return in terms o f true or false
+
+
+
+
+}
